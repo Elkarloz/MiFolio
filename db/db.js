87601f1a5e57bdeebd7b68f94cf502fa0cd7d668
Carlos Mauricio Roa <57810969+Elkarloz@users.noreply.github.com>
2021-07-29 22:23:42 -0500

    edit-db

--- db/db.js
+++ db/db.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql");
 
-const mysqlConnection = mysql.createConnection({
+/* const mysqlConnection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "mifolio",
+}); */
+
+const mysqlConnection = mysql.createConnection({
+  host: "baxeex3gnzlbex2mmthf-mysql.services.clever-cloud.com",
+  user: "uf5ngc0cd8f817go",
+  password: "vAuDLy141vzXSTCvojZz",
+  database: "baxeex3gnzlbex2mmthf",
 });
 
 mysqlConnection.connect(function (err) {
